cebae742a8cc4132299139bec12ee1415b2b56af
mwatz1234 <87551985+mwatz1234@users.noreply.github.com>
2023-03-11 13:00:32 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -92,34 +92,68 @@ echo 'killall -9 cloudflared' >> /etc/services.d/pihole-dot-doh/finish
 chmod -v +x /etc/services.d/pihole-dot-doh/run
 chmod -v +x /etc/services.d/pihole-dot-doh/finish
 
+
 ### Lancache cache domains code below
+mkdir -v /etc/s6-overlay/s6-rc.d/_cachedomainsonboot
+mkdir -v /etc/s6-overlay/s6-rc.d/_cachedomainsonboot/dependencies.d
+echo "" > /etc/s6-overlay/s6-rc.d/_cachedomainsonboot/dependencies.d/pihole-FTL
+echo "oneshot" > /etc/s6-overlay/s6-rc.d/_cachedomainsonboot/type
+echo "#!/command/execlineb
+background { bash -e /usr/local/bin/_cachedomainsonboot.sh }" > /etc/s6-overlay/s6-rc.d/_cachedomainsonboot/up
+
+echo "#!/bin/bash
 # Grabbing the repo
 cd ~
 git clone https://github.com/uklans/cache-domains.git
 
 # Making copies of the files
-mkdir /tmp/cache-domains/scripts/ && cp ~/cache-domains/scripts/create-dnsmasq.sh /tmp/cache-domains/scripts/
-mkdir /tmp/cache-domains/ && cp `find ~/cache-domains -name *.txt -o -name cache_domains.json` /tmp/cache-domains
-mkdir /tmp/cache-domains/scripts/ && cp ~/cache-domains/scripts/create-dnsmasq.sh /tmp/cache-domains/scripts/
+mkdir -v /etc/cache-domains/ && cp `find ~/cache-domains -name *.txt -o -name cache_domains.json` /etc/cache-domains
+mkdir -v /etc/cache-domains/scripts/ && cp ~/cache-domains/scripts/create-dnsmasq.sh /etc/cache-domains/scripts/
+
 
 # Setting up our config.json file
-echo 'cp -n /temp/config.json ~/' >> /etc/services.d/lancache-cache-domains/run
-echo 's6-echo "Copied lancache-cache-domains config.json file"' >> /etc/services.d/lancache-cache-domains/run
-cp ~/config.json /tmp/cache-domains/scripts/
+mkdir -v /etc/cache-domains/config
+cp -n /temp/config.json /etc/cache-domains/config/
 
 # Make bash scripts executable
-chmod -v +x /etc/services.d/lancache-cache-domains/run
-chmod -v +x /tmp/cache-domains/scripts/create-dnsmasq.sh
+chmod -v +x /etc/cache-domains/scripts/create-dnsmasq.sh
 
 # Manually generating our dnsmasq files
-cd /tmp/cache-domains/scripts
+cd /etc/cache-domains/scripts
 ./create-dnsmasq.sh
 
 # Copying our files for Pi-hole to use 
-sudo cp -r /tmp/cache-domains/scripts/output/dnsmasq/*.conf /etc/dnsmasq.d/
+sudo cp -r /etc/cache-domains/scripts/output/dnsmasq/*.conf /etc/dnsmasq.d/
+
+chown -v root:root /etc/cache-domains/*
+chmod -v 644 /etc/cache-domains/*
 
 # Automating the process
-echo 'cp -n /temp/lancache-dns-updates.sh ~/' >> /etc/services.d/lancache-cache-domains/run
-chmod -v +x ~/lancache-dns-updates.sh
-echo 's6-echo "Copied Updater Script File"' >> /etc/services.d/lancache-cache-domains/run
-echo '' >> /etc/cron.d/pihole-updatelists
+cp -n /temp/lancache-dns-updates.sh /usr/local/bin/
+chmod -v +x /usr/local/bin/lancache-dns-updates.sh
+" >  /usr/local/bin/_cachedomainsonboot.sh
+chmod -v +x /usr/local/bin/_cachedomainsonboot.sh
+
+echo "Installed cache-domain files!"
+
+if [ ! -d "/etc/s6-overlay/s6-rc.d/_postFTL" ]; then
+    echo "Missing /etc/s6-overlay/s6-rc.d/_postFTL directory"
+    exit 1
+fi
+
+mkdir -pv /etc/s6-overlay/s6-rc.d/_postFTL/dependencies.d
+echo "" > /etc/s6-overlay/s6-rc.d/_postFTL/dependencies.d/_cachedomainsonboot
+echo "Added dependency to _postFTL service (/etc/s6-overlay/s6-rc.d/_postFTL/dependencies.d/_cachedomainsonboot)!"
+
+if [ ! -f "/etc/cron.d/cache-domains" ]; then
+		echo "# cache-domains Updater by mwatz1234
+# https://github.com/mwatz1234/pihole-dot-doh-updatelists-lancache-cache-domains
+
+#30 4 * * *   root   ~/lancache-dns-updates.sh
+" > /etc/cron.d/cache-domains
+		sed "s/#30 /$((1 + RANDOM % 58)) /" -i /etc/cron.d/cache-domains
+		echo "Created crontab (/etc/cron.d/cache-domains)"
+	fi        
+
+echo "Created crontab line for cache-domains"
+    
