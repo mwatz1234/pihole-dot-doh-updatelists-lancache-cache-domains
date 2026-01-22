ac8a093f34245d505a8d476b2ed4d4b7f45d964b
mwatz1234 <87551985+mwatz1234@users.noreply.github.com>
2026-01-21 19:02:41 -0800

    Update lancache-dns-updates.sh

--- stuff/lancache-dns-updates.sh
+++ stuff/lancache-dns-updates.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
 set -e
 
-### Set variables ###
 GITSYNCDIR=/root/cache-domains
 DNSMASQCONFIG=/etc/cache-domains/config/config.json
 
-# Create a new temp directory
+# Create temp dir
 TEMPDIR=$(mktemp -d)
 if [ ! -e "$TEMPDIR" ]; then
     >&2 echo "Failed to create temp directory"
     exit 1
 fi
-
-# Clean up on exit
 trap 'rm -rf "$TEMPDIR"' EXIT
 
-# Fetch latest from git
+# Pull latest
 cd "$GITSYNCDIR"
 git fetch
 HEADHASH=$(git rev-parse HEAD)
 UPSTREAMHASH=$(git rev-parse master@{upstream})
-
 if [ "$HEADHASH" != "$UPSTREAMHASH" ]; then
     echo "Upstream repo has changed!"
     git pull
@@ -29,13 +25,11 @@ else
     exit
 fi
 
-# Copy domain files and scripts to temp
+# Copy files to temp
 cp $(find "$GITSYNCDIR" -name "*.txt" -o -name "cache_domains.json") "$TEMPDIR"
 mkdir -p "$TEMPDIR/scripts"
 cp "$GITSYNCDIR/scripts/create-dnsmasq.sh" "$TEMPDIR/scripts/"
 chmod +x "$TEMPDIR/scripts/create-dnsmasq.sh"
-
-# Copy config.json to temp
 cp "$DNSMASQCONFIG" "$TEMPDIR/scripts/"
 
 # Generate dnsmasq files
